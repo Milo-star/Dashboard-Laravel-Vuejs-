92e59b1b61642d52e031c8ee0ac25efe63bd80ad
Milo Bonnet <milovtt@gmail.com>
2023-04-25 09:55:04 +0200

    Install VueJs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+
+// install vue 3 
+import { createApp } from 'vue'
+import App from './app.vue'
+
+const app = createApp(App)
+
+app.mount('#dashboard')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Dashboard</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        
+        <!-- Styles -->
+        @Vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="antialiased">
+    <body id="dashboard">
         
     </body>
 </html>
