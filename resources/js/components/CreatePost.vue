bc747d65528c672a3a1b4542e3b805d10945b38a
Milo Bonnet <milovtt@gmail.com>
2023-04-25 14:57:42 +0200

    Page Posts

---
+++ resources/js/components/CreatePost.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-dialog v-model="dialog" max-width="500">
+      <template v-slot:activator="{ props }">
+        <v-btn color="primary" v-bind="props" rounded>Create</v-btn>
+      </template>
+
+      <v-card>
+        <v-card-text>
+            <v-form>
+                <v-text-field label="Title" />
+                <v-text-field label="Slug" />
+                <v-textarea label="Content" />
+                <v-btn color="primary" @click="dialog = false">Create</v-btn>
+            </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" block @click="dialog = false">Cancel</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</template>
+
+<script setup>
+
+import { ref } from 'vue'
+
+const dialog = ref(false)
+
+</script>
--- resources/js/pages/Post.vue
+++ resources/js/pages/Post.vue
@@ -1,7 +1,47 @@
 <template>
-    <v-card rounded>
-        <v-card-title>
-            <h1>Post</h1>
-        </v-card-title>
+    <v-card rounded class="py-8 px-4">
+        <div class="flex">
+            <h1 class="flex-1">Posts</h1>
+            <create-post/>
+        </div>
     </v-card>
+
+    <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">Slug</th>
+        <th class="text-left">Title</th>
+        <th class="text-left">Updated At</th>
+        <th class="text-right">Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="({slug, title, updated_at}, index) in posts"
+        :key="index"
+      >
+        <td>{{ slug }}</td>
+        <td>{{ title }}</td>
+        <td>{{ updated_at }}</td>
+        <td class="flex justify-end items-center gap-2">
+            <v-btn size="small" color="primary" icon="mdi-pencil"></v-btn>
+            <v-btn size="small" color="error" icon="mdi-delete"></v-btn>
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
+
+<script setup>
+
+    import { ref } from 'vue'
+    import CreatePost from '../components/CreatePost.vue'
+
+    const posts = ref([
+        { slug: 'post-1', title: 'Post 1', updated_at: '2023-04-25' },
+        { slug: 'post-2', title: 'Post 2', updated_at: '2023-04-25' },
+        { slug: 'post-3', title: 'Post 3', updated_at: '2023-04-25' },
+    ])
+
+
+</script>
