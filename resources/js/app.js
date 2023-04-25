f8694ec29464cd8f9bde8afcd6e5228c3a89e458
Milo Bonnet <milovtt@gmail.com>
2023-04-25 10:55:45 +0200

    Install Vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,14 @@ import './bootstrap';
 import '../css/app.css';
 
 // install vue 3 
-import { createApp } from 'vue'
-import App from './app.vue'
+import { createApp } from 'vue';
+import router from './plugins/router';
+import App from './app.vue';
+import vuetify from './plugins/vuetify';
 
 const app = createApp(App)
+
+app.use(router)
+app.use(vuetify)
 
 app.mount('#dashboard')
--- resources/js/app.vue
+++ resources/js/app.vue
@@ -1,5 +1,11 @@
 <template>
-    <h1>
-        Hello
-    </h1>
+    <header class="bg-indigo-900 text-white">
+        <nav class="container mc-auto flex justify-center items-center gap-4 ">
+            <router-link :to="{name: 'home'}">Home</router-link>
+            <router-link :to="{name: 'about'}">About</router-link>
+        </nav>
+    </header>
+    <main class="h-screen bg-rose-500/20">
+        <router-view></router-view>
+    </main>
 </template>
