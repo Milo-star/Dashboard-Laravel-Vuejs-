9b00df2459c3326f578548cdc9dd282b54d7278a
Milo Bonnet <milovtt@gmail.com>
2023-05-08 14:51:25 +0200

    Modif page about.vue

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -8,4 +8,21 @@
     <v-card-text>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet incidunt ut repudiandae at atque ipsam? Obcaecati recusandae sit odio quae amet exercitationem eveniet quasi eius tempore, modi, facilis nemo distinctio.
     </v-card-text>
+
+    <v-carousel>
+        <v-carousel-item
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            cover
+        ></v-carousel-item>
+
+        <v-carousel-item
+            src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+            cover
+        ></v-carousel-item>
+
+        <v-carousel-item
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            cover
+        ></v-carousel-item>
+    </v-carousel>
 </template>
