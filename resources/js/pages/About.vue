31f987b7bd8379e7ac47bd010be5dfd936e185b1
Milo Bonnet <milovtt@gmail.com>
2023-05-08 13:47:39 +0200

    add text About

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -4,4 +4,8 @@
             <h1 class="flex-1">About</h1>
         </div>
     </v-card>
+
+    <v-card-text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet incidunt ut repudiandae at atque ipsam? Obcaecati recusandae sit odio quae amet exercitationem eveniet quasi eius tempore, modi, facilis nemo distinctio.
+    </v-card-text>
 </template>
